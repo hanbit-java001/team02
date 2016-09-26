b27c360df575845b57bde5ac0943ba1a86900fb4
혀누 <@DESKTOP-NQIVG0N>
2016-09-26 14:59:17 +0900

    프로젝트 초기 셋팅

---
+++ src/main/java/com/hanbit/team02/web/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.hanbit.team02.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+	
+	@RequestMapping("/")
+	public String welcome(){
+		
+		return "index";
+	}
+
+}
