450a08c083110bee507e1d57ebb3e5ecc9524340
user <user@hb5005>
2016-09-28 15:37:48 +0900

    TicketDAO, TicketVO

--- src/main/java/com/hanbit/team02/web/controller/core/dao/TicketDAO.java
+++ src/main/java/com/hanbit/team02/web/controller/core/dao/TicketDAO.java
@@ -1,5 +1,33 @@
 package com.hanbit.team02.web.controller.core.dao;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.hanbit.team02.web.controller.core.vo.TicketVO;
+
+@Repository
 public class TicketDAO {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(TicketDAO.class);
+
+	@Autowired
+	public int reserveTicket(TicketVO ticket) {
+		return 0;
+	}
+
+	public int cancelTicket(int reservedNumber) {
+		return reservedNumber;
+	}
+
+	public List<TicketVO> selectTickets(int reservedNumber) {
+		return null;
+	}
+
+	public TicketVO selectTicket(int reservedNumber) {
+		return null;
+	}
 }
--- src/main/java/com/hanbit/team02/web/controller/core/vo/TicketVO.java
+++ src/main/java/com/hanbit/team02/web/controller/core/vo/TicketVO.java
@@ -1,5 +1,47 @@
 package com.hanbit.team02.web.controller.core.vo;
 
 public class TicketVO {
+	private int trainId;
+	private int seatNumber;
+	private int reservedNumber;
+	private String name;
+	private int reservedTime;
+	private int cancel;
 
+	public int getTrainId() {
+		return trainId;
+	}
+	public void setTrainId(int trainId) {
+		this.trainId = trainId;
+	}
+	public int getSeatNumber() {
+		return seatNumber;
+	}
+	public void setSeatNumber(int seatNumber) {
+		this.seatNumber = seatNumber;
+	}
+	public int getReservedNumber() {
+		return reservedNumber;
+	}
+	public void setReservedNumber(int reservedNumber) {
+		this.reservedNumber = reservedNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getReservedTime() {
+		return reservedTime;
+	}
+	public void setReservedTime(int reservedTime) {
+		this.reservedTime = reservedTime;
+	}
+	public int getCancel() {
+		return cancel;
+	}
+	public void setCancel(int cancel) {
+		this.cancel = cancel;
+	}
 }
