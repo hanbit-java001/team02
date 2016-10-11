f8d92c25e058dc155afbcb409c5f7ebffac9e4bb
혀누 <@DESKTOP-NQIVG0N>
2016-10-11 17:28:22 +0900

    으아아아아아아아아아아아

--- src/main/java/com/hanbit/team02/core/session/SessionAspect.java
+++ src/main/java/com/hanbit/team02/core/session/SessionAspect.java
@@ -3,34 +3,36 @@ package com.hanbit.team02.core.session;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.ibatis.binding.MapperMethod.MethodSignature;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
+@Order(2)
 public class SessionAspect {
 
-//	@Around("@annotation(com.hanbit.team02.core.session.LoginRequired)")
-//	public Object checkLogin(ProceedingJoinPoint pjp) throws Throwable {
-//
-//		Session session = SessionHelpler.getSession();
-//
-//		if (session.isLoggedIn()) {
-//			return pjp.proceed();
-//		}
-//
-//		MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
-//		Class returnType = methodSignature.getReturnType();
-//
-//		if (returnType == String.class) {
-//			return "login";
-//		}
-//
-//		Map result = new HashMap();
-//		result.put("errorMsg", "로그인이 필요합니다.");
-//		return result;
-//	}
+	@Around("@annotation(com.hanbit.team02.core.session.LoginRequired)")
+	public Object checkLogin(ProceedingJoinPoint pjp) throws Throwable {
+
+		Session session = SessionHelpler.getSession();
+
+		if (session.isLoggedIn()) {
+			return pjp.proceed();
+		}
+
+		MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
+		Class returnType = methodSignature.getReturnType();
+
+		if (returnType == String.class) {
+			return "login";
+		}
+
+		Map result = new HashMap();
+		result.put("errorMsg", "로그인이 필요합니다.");
+		return result;
+	}
 }
--- webapp/WEB-INF/jsp/home/main.jsp
+++ webapp/WEB-INF/jsp/home/main.jsp
@@ -9,13 +9,13 @@
 <link rel="stylesheet" href="/static/plugins/font-awesome/css/font-awesome.min.css">
 <link rel="stylesheet" href="/static/plugins/material/iconfont/material-icons.css">
 <link rel="stylesheet" href="/static/css/main.css" />
+<link rel="stylesheet" href="/static/css/common.css" />
 </head>
 <body>
-	<div class="main-container">
-
-		<div class="main-top">
+	<div class="team02-body">
+		<div class="team2-header">
 			<div class="main-top-body">
-				<div class="main-name">종합 교통 예매</div>
+				<div class="main-name">종합 교통 예매	</div>
 				<div class="main-menu">
 					<ul>
 						<li id="mainHome">홈</li>
@@ -24,17 +24,16 @@
 					</ul>
 				</div>
 			</div>
-		</div>
 
-		<div class="main-content">
-			<div class="main-content-body">
-				<h2>편리한 교통 예매 서비스</h2>
-				<p>목적지로 가는 경로를 추천해주고, 교통편을 예약할 수 있습니다.</p><br>
-				<button id="btnJoin" class="btn btn-primary">지금 가입하기</button>
+			<div class="main-content">
+				<div class="main-content-body">
+					<h2>편리한 교통 예매 서비스</h2>
+					<p>목적지로 가는 경로를 추천해주고, 교통편을 예약할 수 있습니다.</p><br>
+					<button id="btnJoin" class="btn btn-primary">지금 가입하기</button>
+				</div>
 			</div>
 		</div>
 
-		<div class="main-bottom">2016, Hanbit Team 02</div>
 
 		<div class="login-dialog" >
 			<div class="form-group">
@@ -52,6 +51,7 @@
 		</div>
 
 	</div>
+		<div class="team02-footer">2016, Hanbit Team 02</div>
 <script src="/static/plugins/jquery/jquery-3.1.0.min.js"></script>
 <script src="/static/plugins/bootstrap/js/bootstrap.min.js"></script>
 </body>
