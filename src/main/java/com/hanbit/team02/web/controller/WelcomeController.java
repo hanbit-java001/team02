699888574f4a37b3096e9f9dbafb7ca436e2924e
혀누 <@DESKTOP-NQIVG0N>
2016-10-12 16:26:51 +0900

    dd

--- src/main/java/com/hanbit/team02/web/controller/WelcomeController.java
+++ src/main/java/com/hanbit/team02/web/controller/WelcomeController.java
@@ -9,7 +9,7 @@ public class WelcomeController {
 	@RequestMapping("/")
 	public String welcome(){
 		
-		return "/ticketing/ticketing";
+		return "/home/main";
 	}
 	
 	@RequestMapping("/team02")
--- webapp/static/js/main.js
+++ webapp/static/js/main.js
@@ -1,3 +1,14 @@
 $(function(){
+	$(".mainLogin").on("click", function(){
+		hideMainContent();
+		showLoginDialog();
+	})
 	
+	function hideMainContent(){
+		$(".main-content").hide();
+	}
+	
+	function showLoginDialog(){
+		$(".login-dialog").show();
+	}
 });
