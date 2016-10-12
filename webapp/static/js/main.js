ccbbd468e4178a1094186561e1bba5aff6b3cf20
user <user@hb5005>
2016-10-12 16:30:31 +0900

    main.js

--- webapp/static/js/main.js
+++ webapp/static/js/main.js
@@ -3,12 +3,26 @@ $(function(){
 		hideMainContent();
 		showLoginDialog();
 	})
-	
+
 	function hideMainContent(){
 		$(".main-content").hide();
 	}
-	
+
 	function showLoginDialog(){
 		$(".login-dialog").show();
 	}
+
+	$(.main-menu ul li).on("click", function() {
+		var mainId = $(this).attr("id");
+		if(mainId == "mainHome") {
+			location.href = "/home/main";
+		}
+		else if (mainId == "mainService"){
+			location.href = "/ticketing/ticketing";
+		}
+	});
+
+	$("#btnJoin").on("click", function() {
+		location.href = "/member/join";
+	});
 });
