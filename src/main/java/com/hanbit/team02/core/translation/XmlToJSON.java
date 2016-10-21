94dc1a2679820890177ae2cc3bdff5392d34b164
혀누 <@DESKTOP-NQIVG0N>
2016-10-21 17:55:40 +0900

    xml to JSON 변환 class

--- src/main/java/com/hanbit/team02/core/translation/XmlToJSON.java
+++ src/main/java/com/hanbit/team02/core/translation/XmlToJSON.java
@@ -1,60 +1,47 @@
 package com.hanbit.team02.core.translation;
 
-import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
- 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
- 
+import java.net.URLEncoder;
+
+import org.springframework.stereotype.Service;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import net.sf.json.xml.XMLSerializer;
+
+@Service
 public class XmlToJSON {
     
-    public XmlToJSON() throws Exception{
-         
-         JSONParser jsonparser = new JSONParser();
-         JSONObject jsonobject = (JSONObject)jsonparser.parse(readUrl());
-         JSONObject json =  (JSONObject) jsonobject.get("boxOfficeResult");
-         JSONArray array = (JSONArray)json.get("dailyBoxOfficeList");
-         for(int i = 0 ; i < array.size(); i++){
-             
-             JSONObject entity = (JSONObject)array.get(i);
-             String movieNm = (String) entity.get("movieNm");
-             System.out.println(movieNm);
-         }
-         
-         
-    }
-    private static String readUrl() throws Exception {
-        BufferedInputStream reader = null;
-        try {
-            URL url = new URL(
-                    "http://openapi.tago.go.kr/openapi/service/TrainInfoService/"
-                    + "getCtyCodeList.json"
-                    + "?ServiceKey=Wx6wZSSkjgV9%2FZWena%2Fr0NZcwetrNNAbNzQpqxaiOKpgws04rbXLlLyEcfRdUNMUaBQdZPHtcjfZL2HFBdi%2BBQ%3D%3D"
-                    );
-            reader = new BufferedInputStream(url.openStream());
-            StringBuffer buffer = new StringBuffer();
-            int i;
-            byte[] b = new byte[4096];
-            while( (i = reader.read(b)) != -1){
-              buffer.append(new String(b, 0, i));
-            }
-            return buffer.toString();
-        } finally {
-            if (reader != null)
-                reader.close();
+    public String XmlToJson(String operatorName, String searchingCountNum, String pageNum) throws IOException{
+    	StringBuilder urlBuilder = new StringBuilder("http://openapi.tago.go.kr/openapi/service/TrainInfoService/"+operatorName); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=Wx6wZSSkjgV9%2FZWena%2Fr0NZcwetrNNAbNzQpqxaiOKpgws04rbXLlLyEcfRdUNMUaBQdZPHtcjfZL2HFBdi%2BBQ%3D%3D"); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode(searchingCountNum, "UTF-8")); /*검색건수*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode(pageNum, "UTF-8")); /*페이지 번호*/
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        System.out.println("Response code: " + conn.getResponseCode());
+        BufferedReader rd;
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
         }
-    }
- 
-    
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        try {
-            new XmlToJSON();
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
         }
+        rd.close();
+        conn.disconnect();
+        XMLSerializer xmlserializer = new XMLSerializer();
+        JSONObject json = (JSONObject) xmlserializer.read(sb.toString());
+        String jsonString = json.toString();
+        return jsonString;
     }
- 
 }
