9dea2205b8ff267701bb5e17e6e8737c860daa7e
혀누 <@DESKTOP-NQIVG0N>
2016-11-01 18:15:54 +0900

    161101 1st

--- webapp/static/js/stations.js
+++ webapp/static/js/stations.js
@@ -1,4 +1,24 @@
 $(function(){
+	
+	function stationsPaging(obj) {
+		for(var j = 0; j</*obj.body.items.length/*/4; j++){
+			stationTableHTML += '<tr>';
+			for(var i = j*4; i<(j*4)+4; i++){
+				if(i < obj.body.items.length){
+					stationTableHTML += '<td nodeid ="'+ obj.body.items[i].nodeid +'">';
+					stationTableHTML += obj.body.items[i].nodename;
+				}else if(i >= obj.body.items.length){
+					stationTableHTML += '';
+				}
+				stationTableHTML += '</td>';
+			}
+			stationTableHTML += '</tr>';
+		}
+		stationTableHTML += '</table>';
+		stationTableHTML += '<div id="pagingBar">';
+		stationTableHTML += '</div>';
+	}
+	
 	var cityCode = 0;
 	function makeCityTables(caller) {
 		$.ajax({
@@ -14,21 +34,21 @@ $(function(){
 		});	
 	}
 	
-	function makeStationTable(caller) {
+	function makeStationTable(target, cityCode) {
 		$.ajax({
 			url:"/api/get/TrainSttnList",
 			method:"POST",
 			data:{
 				operatorName : "getCtyAcctoTrainSttnList",
-				cityCode : 11,
+				cityCode : cityCode,
 				numOfRows : 999,
 			}
 		}).done(function(result){
-			addStations(result, caller);
+			addStations(result, target);
 		})
 	}
 
-	function addStations(obj, caller) {
+	function addStations(obj, target) {
 		var stationTableHTML = "";
 		stationTableHTML += '<div id="closeIcon">';
 		stationTableHTML += '<i class="material-icons closeIcon">close</i>';
@@ -38,7 +58,7 @@ $(function(){
 		stationTableHTML += '	<th id="departureOrarrival" colspan="4"></th>';
 		stationTableHTML += '</tr>';
 		
-		for(var j = 0; j<obj.body.items.length/4; j++){
+		for(var j = 0; j</*obj.body.items.length/*/4; j++){
 			stationTableHTML += '<tr>';
 			for(var i = j*4; i<(j*4)+4; i++){
 				if(i < obj.body.items.length){
@@ -52,24 +72,67 @@ $(function(){
 			stationTableHTML += '</tr>';
 		}
 		stationTableHTML += '</table>';
+		stationTableHTML += '<div id="pagingBar">';
+		for (var i = 0; i<obj.body.items.length/4/4; i++){
+			stationTableHTML += '<div class="pageNumber';
+			if (i != obj.body.items.length/4/4) {
+				stationTableHTML += ' selectable';
+			}
+			stationTableHTML += '">'+(i+1)+'</div>'
+		}
+		stationTableHTML += '</div>';
 		
 		$("#trainStationsTable").append(stationTableHTML);
 	
 		//표 머리에 출발역 도착역 쓰는 코드
-		var tofrom = caller.attr("tgt");
-		if (tofrom == "departure"){
+		
+		if (target == "departure"){
 			$("#departureOrarrival").text($(".arr").text());
-		} else if (tofrom == "arrival") {
+		} else if (target == "arrival") {
 			$("#departureOrarrival").text($(".dep").text());
 		}
-		$("#trainStationsTable").attr("tgt", tofrom);
+		$("#trainStationsTable").attr("tgt");
 		
 		$("tr td").mouseenter(function(){
-			$(this).css("color", "blue")
+				$(this).css({"color": "blue","font-weight":"bold"})				
 		})
 		$("tr td").mouseleave(function(){
-			$(this).css("color", "black")
+			$(this).css({"color": "black","font-weight":"normal"})
 		})
+		$("tr td").click(function(){
+			var target = $("#trainStationsTable").attr("tgt");
+			
+			if (target == "departure") {
+				$(".form-departure").val($(this).text())
+			}
+			else if (target == "arrival") {
+				$(".form-arrival").val($(this).text())
+			}
+			$("#trainStationsTable").hide()
+			$(".reserveForm").show()
+			$("#closeIcon").detach()
+			$(".stationTable").detach()
+			$("#pagingBar").detach()
+		});
+		
+		$(".pageNumber").click(function(){
+			var selectedNumber = $(".pageNumber").text()
+			if (selectedNumber == (obj.body.items.length/4/4)+1){
+				
+			}else{
+				
+			}
+			$(".stationTable").detach()
+			$("#pagingBar").detach()
+		});
+		
+		
+		$(".closeIcon").click(function(){
+			$("#trainStationsTable").hide()
+			$(".reserveForm").show()
+			$(".stationTable").detach()
+			$("#pagingBar").detach()
+		});
 	}
 
 
@@ -113,37 +176,35 @@ $(function(){
 		$("#trainStationsTable").attr("tgt", tofrom);
 
 		$("tr td").mouseenter(function(){
-			$(this).css("color", "blue")
+			$(this).css({"color": "blue","font-weight":"bold"})
 		})
 		$("tr td").mouseleave(function(){
-			$(this).css("color", "black")
+			$(this).css({"color": "black","font-weight":"normal"})
 		})
 		$("tr td").click(function(){
 			var target = $("#trainStationsTable").attr("tgt");
+//			
+//			if (target == "departure") {
+//				$(".form-departure").val($(this).text())
+//			}
+//			else if (target == "arrival") {
+//				$(".form-arrival").val($(this).text())
+//			}
 			
-			if (target == "departure") {
-				$(".form-departure").val($(this).text())
-			}
-			else if (target == "arrival") {
-				$(".form-arrival").val($(this).text())
-			}
-			
-			cityCode = $("[citycode]").val();
-			makeStationTable($(this));
+			var cityCode = $(this).attr("citycode");
+			makeStationTable(target, cityCode);
 
 			//$("#trainStationsTable").hide()
 			//$(".reserveForm").show()
 			$("#closeIcon").detach()
 			$(".stationTable").detach()	
-			
-			
-		
-			$(".closeIcon").click(function(){
-				$("#trainStationsTable").hide()
-				$(".reserveForm").show()
-				$(".stationTable").detach()
-			});
-		})
+		});
+
+		$(".closeIcon").click(function(){
+			$("#trainStationsTable").hide()
+			$(".reserveForm").show()
+			$(".stationTable").detach()
+		});
 	}
 
 	$(".list-departure, .list-arrival").on("click", function() {
