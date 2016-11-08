ce8da2efb926825898687339b65d2e0b277b1e7b
user <user@hb5005>
2016-11-08 11:10:06 +0900

    회원정보수정

--- webapp/static/js/member.js
+++ webapp/static/js/member.js
@@ -37,6 +37,7 @@ $(function() {
 
 			addMember(name, memberId, email, phoneNumber);
 		}).fail(function() {
+			$(".main-content").empty();
 			alert("로그인이 필요합니다.");
 			location.href = "/home/main";
 		});
@@ -44,8 +45,41 @@ $(function() {
 
 	getMember();
 
+	function getUpdateInfo() {
+		$.ajax({
+			url: "/api/member/viewMember",
+			method: "POST"
+		}).done(function(memberInfo) {
+			var member = memberInfo.info;
+
+			var password = member.password;
+			var email = member.email;
+			var phoneNumber = member.phoneNumber;
+
+			updateMember(password, email, phoneNumber);
+		}).fail(function() {
+			$(".main-content").empty();
+			alert("로그인이 필요합니다.");
+			location.href = "/home/main";
+		});
+	}
+
+	getUpdateInfo();
+
 	$(".btnUpdate").on("click", function() {
-		alert("구현 중입니다.");
+		updateMember();
+	});
+
+	$(".btnUpdateConfirm").on("click", function() {
+		callAjax({
+			url: "/api/member/editMember",
+			method : "POST",
+			success : function(result) {
+				if (result.countEdited > 0) {
+					alert("회원정보가 수정되었습니다.");
+				}
+			}
+		});
 	});
 
 	$(".btnCancel").on("click", function() {
@@ -60,7 +94,7 @@ $(function() {
 				method: "DELETE",
 				success: function(result) {
 					if (result.countRemoved > 0) {
-						alert("탈퇴가 완료되었습니다. 안녕");
+						alert("탈퇴가 완료되었습니다. 그동안 이용해주셔서 감사합니다.");
 					}
 				}
 			});
@@ -68,4 +102,36 @@ $(function() {
 	        location.reload();
 	    }
 	});
+
+	function updateMember(password, email, phoneNumber) {
+		$(".member-container").empty();
+		$(".bottom-buttons").empty();
+
+		var updateHTML = "";
+		updateHTML += '<div class="member-update">';
+
+		updateHTML += '<div class="form-group">';
+		updateHTML += '<label for="txtPassword">'+'비밀번호'+'</label>';
+		updateHTML += '<input type="password" class="form-control" id="txtPassword" value='+password+'>';
+		updateHTML += '</div>';
+		updateHTML += '<div class="form-group">';
+		updateHTML += '<label for="txtPasswordConfirm">'+'비밀번호 확인'+'</label>';
+		updateHTML += '<input type="password" class="form-control" id="txtPasswordConfirm" value='+password+'>';
+		updateHTML += '</div>';
+		updateHTML += '<div class="form-group">';
+		updateHTML += '<label for="txtEmail">'+'이메일'+'</label>';
+		updateHTML += '<input type="text" class="form-control" id="txtEmail" value='+email+'>';
+		updateHTML += '</div>';
+		updateHTML += '<div class="form-group">';
+		updateHTML += '<label for="txtPhoneNumber">'+'전화번호'+'</label>';
+		updateHTML += '<input type="text" class="form-control" id="txtPhoneNumber" value='+phoneNumber+'>';
+		updateHTML += '</div>';
+		updateHTML += '<div class="bottom-buttons">';
+		updateHTML += '<button class="btnUpdateConfirm btn btn-success">확인</button>';
+		updateHTML += '</div>';
+		updateHTML += '</div>';
+
+		$(".member-container").append(updateHTML);
+	}
+
 });
